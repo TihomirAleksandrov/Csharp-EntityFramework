6117d01bfb13fade8deabf3fb14b3f939077aba0
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-06 15:30:25 +0200

    Added new files

---
+++ AdoDotNet-Lab/Demo/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Demo
+{
+    public static class Configuration
+    {
+        public const string CONNECTION_STRING = @"Server=LAPTOP-QV7V977H;Database=MinionsDB;Trusted_Connection=True;Trust Server Certificate=true;";
+    }
+}
---
+++ AdoDotNet-Lab/Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+
+        {
+            SqlConnection conn = new SqlConnection(Configuration.CONNECTION_STRING);
+            conn.Open();
+
+            await using (conn){
+                SqlCommand filterDb = new SqlCommand(Queries.VillainsWithMoreThan3Minions, conn);
+
+                SqlDataReader reader = await filterDb.ExecuteReaderAsync();
+
+                await using (reader)
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        string villainName = reader.GetString(0);
+                        int minionsCount = reader.GetInt32(1);
+
+                        Console.WriteLine($"{villainName} - {minionsCount}");
+                    }
+                }
+            }
+        }
+    }
+}
