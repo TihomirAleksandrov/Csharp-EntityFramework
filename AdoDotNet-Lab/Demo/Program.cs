971dda2b5cede2ae384f7fd70ca9cf80818897f7
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-07 16:43:29 +0200

    Added new task

--- AdoDotNet-Lab/Demo/Program.cs
+++ AdoDotNet-Lab/Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Demo
@@ -12,19 +13,77 @@ namespace Demo
             SqlConnection conn = new SqlConnection(Configuration.CONNECTION_STRING);
             conn.Open();
 
+            int villainId = int.Parse(Console.ReadLine());
+
             await using (conn){
-                SqlCommand filterDb = new SqlCommand(Queries.VillainsWithMoreThan3Minions, conn);
+                await PrintVillainMinionsByVillainIdAsync(conn, villainId);
+            }
+        }
 
-                SqlDataReader reader = await filterDb.ExecuteReaderAsync();
+        //Task2
+        private static async Task PrintVillainsWithMoreThan3MinionsAsync(SqlConnection conn)
+        {
+            SqlCommand filterDb = new SqlCommand(Queries.VillainsWithMoreThan3Minions, conn);
 
-                await using (reader)
+            SqlDataReader reader = await filterDb.ExecuteReaderAsync();
+
+            await using (reader)
+            {
+                while (await reader.ReadAsync())
                 {
+                    string villainName = reader.GetString(0);
+                    int minionsCount = reader.GetInt32(1);
+
+                    Console.WriteLine($"{villainName} - {minionsCount}");
+                }
+            }
+        }
+
+        //Task3
+        private static async Task PrintVillainMinionsByVillainIdAsync(SqlConnection conn, int villainId)
+        {
+            SqlCommand commandVillainId = new SqlCommand(Queries.VillainNameById, conn);
+            commandVillainId.Parameters.AddWithValue("@Id", villainId);
+
+            object villainNameObject = await commandVillainId.ExecuteScalarAsync();
+
+            if (villainNameObject == null)
+            {
+                Console.WriteLine($"No villain with ID {villainId} exists in the database.");
+                return;
+            }
+
+            string villainName = (string)villainNameObject;
+
+            SqlCommand commandVillainMinions = new SqlCommand(Queries.VillainWithMinionsInfo, conn);
+            commandVillainMinions.Parameters.AddWithValue("@Id", villainId);
+
+            SqlDataReader reader = await commandVillainMinions.ExecuteReaderAsync();
+
+            await using (reader)
+            {
+                Console.WriteLine($"Villain: {villainName}");
+
+                if (!reader.HasRows)
+                {
+                    Console.WriteLine("(no minions)");
+                }
+                else
+                {
+                    List<string> minionsCollection = new List<string>();
+
                     while (await reader.ReadAsync())
                     {
-                        string villainName = reader.GetString(0);
-                        int minionsCount = reader.GetInt32(1);
+                        long rowNumber = reader.GetInt64(0);
+                        string minionName = reader.GetString(1);
+                        int minionAge = reader.GetInt32(2);
 
-                        Console.WriteLine($"{villainName} - {minionsCount}");
+                        minionsCollection.Add($"{rowNumber}. {minionName} {minionAge}");
+                    }
+
+                    foreach (var minion in minionsCollection)
+                    {
+                        Console.WriteLine(minion);
                     }
                 }
             }
--- AdoDotNet-Lab/Demo/Queries.cs
+++ AdoDotNet-Lab/Demo/Queries.cs
@@ -8,5 +8,15 @@ JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
 GROUP BY v.Id, v.Name
 HAVING COUNT(mv.VillainId) > 3
 ORDER BY COUNT(mv.VillainId)";
+
+        public const string VillainNameById = @"SELECT Name FROM Villains WHERE Id = @Id";
+
+        public const string VillainWithMinionsInfo = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
     }
 }
