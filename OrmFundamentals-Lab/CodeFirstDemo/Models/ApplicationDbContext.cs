334958f9e1881a5ea553308bd7c83301e62ea061
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-08 19:09:54 +0200

    Added new files

---
+++ OrmFundamentals-Lab/CodeFirstDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-QV7V977H;Database=CodeFirstDemoDb;Trusted_Connection=True;Trust Server Certificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<News> News { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ OrmFundamentals-Lab/CodeFirstDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using CodeFirstDemo.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CodeFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ApplicationDbContext();
+            db.Database.EnsureCreated();
+
+            db.Categories.Add(new Category
+            {
+                Title = "Sport",
+                News = new List<News>
+                {
+                    new News
+                    {
+                        Title = "Charles Leclerc is 2023 drivers champion",
+                        Content = "Шарл Льоклер върна титлата при Ферари за пръв път от 2007.",
+                        Comments = new List<Comment>
+                        {
+                            new Comment { Author = "Pierre", Content = "I like this"},
+                            new Comment { Author = "Sbinalla", Content = "Ferrari master Blan"}
+                        }
+                    }
+                }
+            });
+            db.SaveChanges();
+        }
+    }
+}
