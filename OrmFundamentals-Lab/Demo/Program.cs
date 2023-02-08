ccc52af6999edafe28bf02596c1309256c568853
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-08 19:10:09 +0200

    Added new files

---
+++ OrmFundamentals-Lab/Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Demo.Models;
+using System;
+using System.Linq;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+            var employees = db.Employees
+                .Where(x => x.FirstName.StartsWith("N"))
+                .OrderByDescending(x => x.Salary)
+                .Select(x => new {x.FirstName, x.LastName, x.Salary})
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.Salary:f2}$");
+            }
+        }
+    }
+}
