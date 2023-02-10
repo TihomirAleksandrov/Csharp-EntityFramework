7f5626e564082e3df0202ab80d56af9e41472bdc
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-10 16:24:28 +0200

    Added new files

---
+++ OrmFundamentals-Lab/MiniOrmApp/Data/SoftUniDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MiniORM;
+using MiniOrmApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniOrmApp.Data
+{
+    public class SoftUniDbContext : DbContext
+    {
+        public SoftUniDbContext(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; }
+
+        public DbSet<Department> Departments { get; }
+
+        public DbSet<Project> Projects { get; }
+
+        public DbSet<EmployeeProject> EmployeeProjects { get; }
+    }
+}
