df20e36635c98193bbe8b73d6ed75002b5fe1577
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-02-12 20:04:35 +0200

    Added new files

---
+++ EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            using (context)
+            {
+                Console.WriteLine(GetEmployeesFullInformation(context));
+            }
+        }
+
+
+        //Task3
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            var employees = context.Employees
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
