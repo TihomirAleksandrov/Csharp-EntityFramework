f67995c70fdb9ea81156b42f9252e966dc65f426
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-28 13:08:36 +0300

    Added new files

---
+++ EntityRelations-Exercise/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=LAPTOP-QV7V977H;Database=FootballBettingSystem;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
