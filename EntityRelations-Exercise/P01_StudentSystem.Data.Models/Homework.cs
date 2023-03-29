33ca909068d5401801697f8b42ff6ddec48266c2
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-29 11:45:35 +0300

    Added new files

---
+++ EntityRelations-Exercise/P01_StudentSystem.Data.Models/Homework.cs
@@ -0,0 +1,32 @@
+﻿using P01_StudentSystem.Data.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(255)")]
+        public string Content { get; set; }
+
+        public ContentType ContentType { get; set; }
+
+        [Required]
+        public DateTime SubmissionTime { get; set; }
+
+        [ForeignKey(nameof(Student))]
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
