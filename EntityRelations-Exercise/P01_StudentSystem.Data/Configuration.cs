0e6c18e2e1a132572abfeee7be4dfe957398d56f
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-29 11:45:14 +0300

    Added new files

---
+++ EntityRelations-Exercise/P01_StudentSystem.Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace P01_StudentSystem.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=LAPTOP-QV7V977H;Database=StudentSystem;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
