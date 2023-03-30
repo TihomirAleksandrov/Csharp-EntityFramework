e99419250ad929b22b7969deb6506974890b3173
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-03-30 15:49:22 +0300

    Added new files

---
+++ LINQ-Exercise/MusicHubDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicHubDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
